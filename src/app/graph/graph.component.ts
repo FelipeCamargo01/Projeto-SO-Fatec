d81ea6a00c08cf146c8141fc243a1fbc46fa4ff8
feluiz <llf3628@gmail.com>
2021-04-05 22:20:36 -0300

    Alterando valores

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -10,14 +10,59 @@ import { ZingchartAngularModule } from 'zingchart-angular';
 
 export class GraphComponent implements OnInit {
 
+  public valores: Array<any> = [];
+  public valoresGrafico: Array<number> = []; 
+
+  public configZingChart = {};
+
+  // config:zingchart.graphset = this.configZingChart;
   config:zingchart.graphset = {
     type: 'hbar',
     series: [{
-      values: [3,6,4,6,4,6,4,6, 12]
+      values: this.valoresGrafico
     }],
   };
 
-  constructor() { }
+  constructor() { 
+
+    var menu = prompt('Digite a opção desejada \n 1 - Inserir \n 2 - Mostrar \n 3 - FIFO');
+
+    if(menu == '1') {
+      var nome = prompt('Digite o nome');
+      var valor = prompt('Digite o valor');
+
+      var valoresObj = {
+        nome1234: nome,
+        valor1234: valor,
+      }
+
+      console.log('valoresObj ' + valoresObj.nome1234);
+
+      this.valores.push(valoresObj);
+
+      console.log('this.valores > ' + this.valores[0].nome1234);
+
+      console.log(this.valores.length);
+
+      this.valores.forEach(element => {
+        console.log(element);
+      });
+
+      let valoresGrafico = [2,3,5,6,7];
+
+      this.valoresGrafico = valoresGrafico; 
+
+      this.configZingChart = { type: 'hbar',
+        series: [{
+          values: this.valoresGrafico
+        }] 
+      }
+    }
+    else {
+      alert('Opção inválida');
+    }
+
+   }
 
   ngOnInit(): void {
   }
